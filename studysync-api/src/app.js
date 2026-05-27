4dac49a50e8c0e7120401723a33ecfdab6936c27
Juan Carlos Vargas <juancarlosvargasnoviembre10111@gmail.com>
2026-05-26 23:35:02 -0400

    feat: StudySync API completa con CRUD, Redis y Socket.io

--- studysync-api/src/app.js
+++ studysync-api/src/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-//const { pub, sub } = require('./redis/client');
+const { pub, sub } = require('./redis/client');
 const express = require('express');
 const app = express();
 
